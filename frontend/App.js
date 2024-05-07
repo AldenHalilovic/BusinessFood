23ad988a0a2d1eebc4e9802fae89508418c5fd35
Alden Halilovic <Aldehalilovic@gmail.com>
2024-05-07 17:21:53 +0200

    54 pushes let goooo

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>The start</Text>
+      <Text>The start of something great?</Text>
       <StatusBar style="auto" />
     </View>
   );
